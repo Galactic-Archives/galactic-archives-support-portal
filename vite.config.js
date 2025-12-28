7f64d86b22c6468d5412bc9c25c4e56f7dd1e246
Galactic Archives Hackathon <admiral@galacticarchives.space>
2025-12-28 15:06:36 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// Force rebuild
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-      },
+    port: 3000
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
 })
