b3090e0d2e6a829181fe2b3c44c3e168b089acf5
Galactic Archives Hackathon <admiral@galacticarchives.space>
2025-12-31 01:37:35 +0000

    Fix LoginPage imports - correct paths

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
-import { supabase } from './utils/supabase'
+import { supabase } from '../utils/supabase'
 import './Pages.css'
 
 export default function LoginPage() {
